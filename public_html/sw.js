2dcbee979d22784ddf753e3c5e816618e374a0f7
Allan Kukral <me@allankukral.com>
2019-08-07 14:23:42 -0500

    i forgot the files like always

---
+++ public_html/sw.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var cacheVersion = 7;
+var currentCache = {
+  offline: 'offline-cache' + cacheVersion
+};
+const offlineUrl = '/offline.html';
+
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache.offline).then(function (cache) {
+      return cache.addAll([
+        '/ui/css/main.css',
+        // '/ui/js/main.js',
+        '/critical.css',
+        '/ui/webfonts/helvetica/helveticaneue-roman-webfont.woff',
+        '/ui/webfonts/helvetica/helveticaneue-bold-webfont.woff',
+        offlineUrl
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', event => {
+  // request.mode = navigate isn't supported in all browsers
+  // so include a check for Accept: text/html header.
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+    event.respondWith(
+      fetch(event.request.url).catch(error => {
+        // Return the offline page
+        return caches.match(offlineUrl);
+      })
+    );
+  } else {
+    // Respond with everything else if we can
+    event.respondWith(caches.match(event.request)
+      .then(function (response) {
+        return response || fetch(event.request);
+      })
+    );
+  }
+});
